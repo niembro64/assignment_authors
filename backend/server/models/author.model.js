a918915b863766358533ef485ce0a7db714c8b60
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-24 20:15:47 -0400

    css adjusting

--- backend/server/models/author.model.js
+++ backend/server/models/author.model.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
+const minLength = 3;
+
 const AuthorSchema = new mongoose.Schema({
-  name: String,
+  // name: String,
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    // minLength: [minLength, "Name must be at least 3 characters"],
+    minLength: [minLength, "Name must be at least ${minLength} characters"],
+  },
+  // minLength: [minLength, `Name must be at least ${minLength}`]},
 });
 
 const Author = mongoose.model("Author", AuthorSchema);
